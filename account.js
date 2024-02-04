1623aeaaf33702f797e6338623f1a3fe85d10362
odilboooy10 <odilkhon318@gmail.com>
2024-02-04 15:28:54 +0900

    Fixed

--- account.js
+++ account.js
@@ -1,46 +1,47 @@
 const moment = require("moment");
-class Account {#
-    amount;#
-    account_id;
-
-    constructor(name, amount, account_id) {
-        this.name = name;
-        this.#amount = amount;
+class Account {
+    // state
+    #amount
+    #account_id
+    // constructor
+    constructor (account_id, amount, name, currency = "$" ) {
         this.#account_id = account_id;
-    }
+        this.#amount = amount;
+        this.name = name;
+        this.currency = currency;
+    };
 
-    tellMeBalance() {
-        console.log(`Sizning hisobingiz qoldiq:, ${this.#amount}$`);
+    tellMemyBallance() {
+        console.log(`Sizning hisobingizda: ${this.#amount}` + this.currency +` mavjud`);
         return this.#amount;
-    }
+    };
 
     withdrawMoney(amount) {
-        if (this.#amount > amount) {
-            this.#amount -= amount;
-            console.log(`Hisobdan ${amount} yechildi va qoldiq ${this.#amount}$`);
-        } else {
-            console.log(`Sizning balansingizdagi pul yetarli emas: ${this.#amount}$`);
+        if (this.#amount > amount ) {
+            this.#amount -= amount; 
+            console.log(`Sizning hisobingizdan: ${amount}${this.currency} yechildi,   ${this.#amount}${this.currency} qoldi`);
+        }else {
+            console.log(`Sizning hisobingizda mablag etarli emas: ${this.#amount}${this.currency} bor`);
         }
-    }
+    };
 
-    makeDeposit() {
+    makeDeposit(amount) {
         this.#amount += amount;
-        console.log(`Hisobingiz toldirildi, hisobingiz ${this.#amount}$ tashkil etdi`);
-    }
+        console.log(`Sizning xisobingiz ${amount}${this.currency} ga toldirildi `);
+        console.log(`Sizning xisobingiz jami ${this.#amount}${this.currency}`);
+    };
 
     giveMeDetails() {
-        console.log(`Salom hurmatli ${this.name} sizning hisobingiz ${this.#amount}$ ga teng`);
-        console.log("Hisob raqamingiz:", this.#account_id);
-    }
-
+        console.log(`Salom ${this.name} sizning Id raqamingiz ${this.#account_id},va sizning xisobingizda ${this.#amount}${this.currency} mavjud `);
+        console.log(`Sizning xisob raqamingiz:`,this.#account_id);
+    };
     static tellMeAboutClass() {
-        console.log('Bu clasas accountlarni yasash uchun hizmat qiladi');
+        console.log('Bu class Accountlarni yasaydi');
     }
-
-    static tellMeTime() {
-        console.log(`Hozirgi vaqt ${moment().format('YYYY MM DD HH:mm:ss')}`);
+    static tellMeTime() { 
+        console.log(`Hozirgi vaqt`,moment().format('HH:mm:ss'));
     }
-
 }
+const time = Account.tellMeTime() 
 
 module.exports = Account;
